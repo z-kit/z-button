f720a0c199faa7e8f520855a97e5bd5773f0794d
Matheus Kautzmann <mkvchain@gmail.com>
2017-03-01 20:49:42 -0300

    Add tests

---
+++ test/regression/suite/component.js
@@ -0,0 +1,114 @@
+import { test } from 'ava';
+import Nightmare from 'nightmare';
+
+test('SFC component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render button with Click me! text';
+  const expected = 'Click me!';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
+    .wait('.z-button')
+    .evaluate(() => document.querySelector('.z-button').textContent)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - icon', (t) => {
+  t.plan(1);
+  const msg = 'should render circular button';
+  const expected = '100%';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=icon')
+    .wait('.z-button')
+    .evaluate(() => window.getComputedStyle(document.querySelector('.z-button')).borderRadius)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - rounded', (t) => {
+  t.plan(1);
+  const msg = 'should render rounded button';
+  const expected = '96px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=rounded')
+    .wait('.z-button')
+    .evaluate(() => window.getComputedStyle(document.querySelector('.z-button')).borderRadius)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - block', (t) => {
+  t.plan(1);
+  const msg = 'should render block button';
+  const expected = true;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=block')
+    .wait('.z-button')
+    .evaluate(() => {
+      const containerStyle = window.getComputedStyle(document.querySelector('body > div'));
+      const buttonStyle = window.getComputedStyle(document.querySelector('.z-button'));
+      return containerStyle.width === buttonStyle.width;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - bordered', (t) => {
+  t.plan(1);
+  const msg = 'should render bordered button';
+  const expected = true;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=bordered')
+    .wait('.z-button')
+    .evaluate(() => {
+      const buttonStyle = window.getComputedStyle(document.querySelector('.z-button'));
+      const { backgroundColor, borderColor } = buttonStyle;
+      return (backgroundColor === 'rgba(0, 0, 0, 0)' && borderColor !== 'rgba(0, 0, 0, 0)');
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - flat', (t) => {
+  t.plan(1);
+  const msg = 'should render flat button';
+  const expected = true;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=flat')
+    .wait('.z-button')
+    .evaluate(() => {
+      const buttonStyle = window.getComputedStyle(document.querySelector('.z-button'));
+      const { backgroundColor, borderColor } = buttonStyle;
+      return (backgroundColor === 'rgba(0, 0, 0, 0)' && borderColor === 'rgba(0, 0, 0, 0)');
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - disabled state', (t) => {
+  t.plan(1);
+  const msg = 'should render disabled button';
+  const expected = 0;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=disabled%20state')
+    .wait('.z-button')
+    .evaluate(() => {
+      const buttonStyle = window.getComputedStyle(document.querySelector('.z-button'));
+      const { opacity } = buttonStyle;
+      return parseInt(opacity, 10);
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - link button', (t) => {
+  t.plan(1);
+  const msg = 'should render link button';
+  const expected = 'a';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=link')
+    .wait('.z-button')
+    .evaluate(() => document.querySelector('.z-button').tagName.toLowerCase())
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
---
+++ test/unit/suite/index.js
@@ -0,0 +1,114 @@
+import test from 'ava';
+import React from 'react';
+import { renderJSX, JSX } from 'jsx-test-helpers';
+import button from '../../../src/component';
+
+const ZButton = button(React.createElement);
+
+test('JSX component', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<ZButton>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Other props', (t) => {
+  const msg = 'should pass through';
+  const actual = renderJSX(<ZButton disabled>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button" disabled>Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Secondary modifier', (t) => {
+  const msg = 'should be supported';
+  const actual = renderJSX(<ZButton secondary>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--secondary">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Success modifier', (t) => {
+  const msg = 'should be supported';
+  const actual = renderJSX(<ZButton success>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--success">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Warning modifier', (t) => {
+  const msg = 'should be supported';
+  const actual = renderJSX(<ZButton warning>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--warning">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Danger modifier', (t) => {
+  const msg = 'should be supported';
+  const actual = renderJSX(<ZButton danger>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--danger">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Icon button', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<ZButton icon>i</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--icon">i</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Rounded button', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<ZButton rounded>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--rounded">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Block button', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<ZButton block>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--block">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Bordered button', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<ZButton bordered>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--bordered">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Flat button', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<ZButton flat>Click me!</ZButton>);
+  const expected = JSX(
+    <button className="z-button z-button--flat">Click me!</button>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Link button', (t) => {
+  const msg = 'should render `a` tag';
+  const actual = renderJSX(<ZButton link>Click me!</ZButton>);
+  const expected = JSX(
+    <a className="z-button">Click me!</a>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
